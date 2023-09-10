7c34d48edefe4ad84971bf47ee2059a03af99012
GutberBlanco <100219714+GutberBlanco@users.noreply.github.com>
2023-09-10 19:24:19 -0400

    Delete src directory
    arreglando fallo

--- src/main/java/com/springb/apiconn/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.springb.apiconn.repository;
-
-import com.springb.apiconn.model.Users;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<Users, Long> {
-}
--- src/main/java/com/springb/apiconn/repository/WeatherRepository.java
+++
@@ -1,9 +0,0 @@
-package com.springb.apiconn.repository;
-
-import com.springb.apiconn.model.Weather;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface WeatherRepository extends JpaRepository<Weather, Long> {
-}
